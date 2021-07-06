aae28fd817d2ac3ec60c058ea4606b2011204565
dsulliv4 <dsulliv4@binghamton.edu>
2021-07-06 05:07:39 -0400

    js functions

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const stepOne = () => {
     const stepTwoDiv = document.querySelector("#stepTwo");
     const stepOneDiv = document.querySelector("#stepOne");
     stepTwoDiv.classList.remove("hideEle");
-    stepOneDiv.classList.add('hideEle');
-
+    stepOneDiv.classList.add("hideEle");
   });
 };
 
@@ -20,55 +19,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const readyNoButton = () => {
   const button = document.querySelector("#no");
-    button.addEventListener("click", () => {
-     document.querySelector('#tobiasCrying').classList.remove("hideEle")
-     document.querySelector('#returnBtn').classList.remove("hideEle")
-     const stepOneDiv = document.querySelector("#stepOne");
-     stepOneDiv.classList.add('hideEle');
-    })
+  button.addEventListener("click", () => {
+    document.querySelector("#tobiasCrying").classList.remove("hideEle");
+    document.querySelector("#returnBtn").classList.remove("hideEle");
+    const stepOneDiv = document.querySelector("#stepOne");
+    stepOneDiv.classList.add("hideEle");
+  });
 };
 
 const audioPLay = () => {
-    const audio = document.querySelector('audio');
-
-}
-
-
+  const audio = document.querySelector("audio");
+};
 
 const getQuestions = () => {
-  fetch('http://localhost:3000/questions')
-  .then(res => res.json() )
-  .then(data => {
-    let question = "";
-    data.map(function(qInfo){
-      question += `<div> <h3> ${qInfo.content } </h3> </div> `
-    })
-    let qContainer=  document.getElementById('quiz-container');
-    qContainer.innerHTML= question;
-  })
-}
+  fetch("http://localhost:3000/api/v1/questions")
+    .then((res) => res.json())
+    .then((data) => {
+      let question = "";
+      data.map(function (qInfo) {
+        question += `<div> <h3> ${qInfo.content} </h3> </div> `;
+      });
+      let qContainer = document.getElementById("question-container");
+      qContainer.innerHTML = question;
+    });
+};
 
 const continueBtn = () => {
-  const button = document.querySelector('#continue');
+  const button = document.querySelector("#continue");
   button.addEventListener("click", () => {
     getQuestions();
-
+    var hideRules = document.getElementById("stepTwo");
+    console.log(hideRules);
+    hideRules.classList.add("hideEle");
   });
-}
+};
 
 const returnMain = () => {
-  const button = document.querySelector("#returnMain")
+  const button = document.querySelector("#returnMain");
   button.addEventListener("click", () => {
-    window.location = 'index.html';
+    window.location = "index.html";
   });
-
-
-}
+};
 
 const exitMain = () => {
-  const button = document.querySelector("#quit")
+  const button = document.querySelector("#quit");
   button.addEventListener("click", () => {
-    window.location = 'index.html';
+    window.location = "index.html";
   });
-}
-
+};
