9b811eab42255d3bbc0946ec1942fba80e8b2409
dsulliv4 <dsulliv4@binghamton.edu>
2021-08-02 03:18:26 -0400

    render questions

--- src/question.js
+++ src/question.js
@@ -10,3 +10,40 @@ class Question {
    }
   
 }
+
+renderQuestion(question) {
+  DOMElements.quiz_container.style.display = "table";
+  const q = document.createElement("p");
+
+  q.innerHTML = `${question.question_number}. ${question.content}`;
+  DOMElements.quiz_form.appendChild(q);
+
+  for (let i = 1; i < 4; i++) {
+    const div = document.createElement("div");
+    const answer = document.createElement("input");
+    const answer_text = document.createElement("label");
+
+    answer.setAttribute("type", "radio");
+    answer.setAttribute("name", question.id);
+    answer.setAttribute("class", "form-check-input");
+    answer.setAttribute("id", "user_input");
+    answer_text.className = "form-check-label";
+
+
+    switch (i) {
+      case 1:
+        answer.setAttribute("value", "A");
+        answer_text.innerText = ` ${question.answer_a}`;
+        break;
+      case 2:
+        answer.setAttribute("value", "B");
+        answer_text.innerText = ` ${question.answer_b}`;
+        break;
+      case 3:
+        answer.setAttribute("value", "C");
+        answer_text.innerText = ` ${question.answer_c}`;
+        break;
+
+  
+    };
+
