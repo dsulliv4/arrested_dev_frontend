6920dca807a5e1957d59e3bb4f770c9c99b29a4e
dsulliv4 <dsulliv4@binghamton.edu>
2021-11-30 23:43:00 -0500

    display questions througgh roundView

--- src/questionView.js
+++ src/questionView.js
@@ -1,7 +1,7 @@
 class QuestionView {
   // TODO
   constructor(question) {
-    this.quesiton = question;
+    this.question = question;
   }
 
   // TODO
@@ -9,14 +9,24 @@ class QuestionView {
     // 1 make an html container for the question data
     // 2 add the question data to the container
     // 3 return the container
-    
-    let html = `<div>
-                    <span>${this.question.content}</span>
-                    ${this.question.answer_1}<input type="radio" value="1"/>
-                    ${this.question.answer_2}<input type="radio" value="2"/>
-                    ${this.question.answer_3}<input type="radio" value="3"/>
-                <div>`;
 
-   return html
+    let html = `
+      <fieldset>
+        <div>${this.question.content}</div>
+
+        <label>
+          ${this.question.answer_a} <input type="radio" value="a" name="question_${this.question.id}_answer"/>
+        </label>
+        <label>
+        ${this.question.answer_b} <input type="radio" value="b" name="question_${this.question.id}_answer"/>
+        </label>
+        <label>
+        ${this.question.answer_c} <input type="radio" value="c" name="question_${this.question.id}_answer" />
+        </label>
+      </fieldset>`;
+
+    return html;
   }
 }
+
+export default QuestionView;
--- src/roundView.js
+++ src/roundView.js
@@ -1,12 +1,23 @@
+import QuestionView from './questionView.js'
+
 class RoundView {
   constructor(round) {
-    this.round = round
+    this.round = round;
   }
-  render(){
-    
-    return `<form> "hello" </form>`
+  render() {
+    const form = `<form class=question-form>
+    <h1> ${this.round.title} </h1>
+    ${this.round.questions
+      .map((question) => {
+        return new QuestionView(question).render();
+      })
+      .join('')}
+  </form> 
+     `;
+
+    return form;
   }
 }
 
-// 
-export default RoundView 
+//
+export default RoundView;
